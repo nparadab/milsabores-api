3bd6838c697953b39e3fdb3f409f70c15a63991b
Nicolás Fabián Parada Bahamondes <n.paradab@duocuc.cl>
2025-11-22 19:56:03 -0300

    Instalación de Java 21 en Render con Adoptium

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Instalar Java 17
-curl -L https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.13%2B11/OpenJDK17U-jdk_x64_linux_hotspot_17.0.13_11.tar.gz -o java.tar.gz
+# Instalar Java 21 desde Adoptium
+curl -L https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.1%2B12/OpenJDK21U-jdk_x64_linux_hotspot_21.0.1_12.tar.gz -o java.tar.gz
 mkdir -p java
 tar -xzf java.tar.gz -C java --strip-components=1
 export JAVA_HOME=$PWD/java
