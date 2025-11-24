55d6c345aa0d4fff66b24308a1d529a259a8940e
nparadab <n.paradab@duocuc.cl>
2025-11-24 15:42:58 -0300

    Update UsuarioRepository.java

--- src/main/java/com/milsabores/repository/UsuarioRepository.java
+++ src/main/java/com/milsabores/repository/UsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmail(String email);
+    Optional<Usuario> findByEmailIgnoreCase(String email);
 }
